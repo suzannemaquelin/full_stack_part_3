d155404dbc278719a93347a0807e10e11b898aaa
Suzanne Maquelin <slmaquelin@gmail.com>
2020-05-15 14:26:26 +0200

    3.10

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
 morgan.token('body', (req) => {
     console.log(req.body)
@@ -11,6 +12,8 @@ const app = express()
 app.use(express.json())
 
 app.use(morgan('tiny'))
+
+app.use(cors())
 
 let persons = [
    {
@@ -96,7 +99,7 @@ app.post('/api/persons', (request,response) => {
     response.json(person)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
